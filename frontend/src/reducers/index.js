99b99275a5c9928acbe2acee7c043b6d85c02f72
Bhoomika Sahu <bhoomikasahu0010@gmail.com>
2022-06-09 22:45:09 +0530

    Implemented User Profile Screen

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,19 +1,31 @@
 import { combineReducers } from "redux";
 import { cartReducer } from "./cartReducer.js";
-import { orderCreateReducer, orderDetailsReducer, orderMineListReducer, orderPayReducer } from "./orderReducers.js";
-import {productDetailsReducer, productListReducer} from "./productReducer.js";
-import { userRegisterReducer, userSigninReducer } from "./userReducers.js";
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderMineListReducer,
+  orderPayReducer,
+} from "./orderReducers.js";
+import { productDetailsReducer, productListReducer } from "./productReducer.js";
+import {
+  userDetailsReducer,
+  userRegisterReducer,
+  userSigninReducer,
+  userUpdateProfileReducer,
+} from "./userReducers.js";
 
 const rootReducer = combineReducers({
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-    cart: cartReducer,
-    userSignin: userSigninReducer,
-    userRegister: userRegisterReducer,
-    orderCreate: orderCreateReducer,
-    orderDetails : orderDetailsReducer,
-    orderPay: orderPayReducer,
-    orderMineList: orderMineListReducer,
-})
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+  userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  orderMineList: orderMineListReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer
+});
 
 export default rootReducer;
